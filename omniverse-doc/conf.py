f46aa084b07106a06451d3985603e722a1260170
Season <73148791+spoonnvidia@users.noreply.github.com>
2021-08-21 01:52:23 +0800

    Add files via upload

--- omniverse-doc/conf.py
+++ omniverse-doc/conf.py
@@ -28,10 +28,11 @@ author = 'spoonnvidia'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+        "sphinx_book_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
